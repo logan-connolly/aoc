777a7ea6e0b35f420aea036484adc5afad439fc2
Logan Connolly <logan.connolly@protonmail.com>
2020-12-05 13:47:59 +0100

    Move main app to app module and test

---
+++ aoc/app.py
@@ -0,0 +1,20 @@
+import argparse
+
+from aoc import year_2020
+
+
+def parse_args(args):
+    """Parse command line arguments from user"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--year", "-y", help="Get solutions for year")
+    parser.add_argument("--day", "-d", help="Get solutions for day")
+    return parser.parse_args(args)
+
+
+def get_solutions(args):
+    """Based on user input, get requested solutions"""
+    solutions = {"2020": year_2020.run}
+    try:
+        solutions[args.year](args.day)
+    except KeyError:
+        print(f"No solutions for {args.year!r}")
