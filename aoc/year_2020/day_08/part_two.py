b69e68cc806e3d0ccb1f16e9c4803816bc3beb8b
Logan Connolly <logan.connolly@protonmail.com>
2020-12-12 12:06:45 +0100

    Update functions to get 100% cov

--- aoc/year_2020/day_08/part_two.py
+++ aoc/year_2020/day_08/part_two.py
@@ -1,11 +1,12 @@
 import copy
-from typing import List
-from .parse import parse_command, Cmd
+from typing import List, Union
+
 from .accumulate import loop_accumulate
+from .parse import Cmd, parse_command
 
 
-def solve(commands: List[Cmd]) -> int:
-    """Day 08: Handheld Halting (part 1)"""
+def solve(commands: List[Cmd]) -> Union[int, None]:
+    """Day 08: Handheld Halting (part 2)"""
     parsed = [parse_command(cmd) for cmd in commands]
     last_cmd_index = len(parsed) - 1
     swapable = [i for i, cmd in enumerate(parsed) if cmd[0] in ("nop", "jmp")]
@@ -17,4 +18,3 @@ def solve(commands: List[Cmd]) -> int:
         idx, acc = loop_accumulate(commands, idx_set=set(), values=(0, 0))
         if idx > last_cmd_index:
             return acc
-    return -1
--- aoc/year_2020/day_09/finder.py
+++ aoc/year_2020/day_09/finder.py
@@ -1,11 +1,10 @@
-from typing import List
+from typing import List, Union
 
 
-def find_weakness(nums: List[int], target: int) -> int:
+def find_weakness(nums: List[int], target: int) -> Union[int, None]:
     """Find a subset that matches target"""
     for idx, _ in enumerate(nums):
         for end_idx in range(idx, len(nums) - 1):
             subset = nums[idx:end_idx]
             if sum(subset) == target:
                 return min(subset) + max(subset)
-    return -1
