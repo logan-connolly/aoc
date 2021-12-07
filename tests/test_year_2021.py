4426d3135048aeb0f64b8e07a5185861cc422bba
Logan Connolly <logan.connolly@protonmail.com>
2021-12-07 10:00:00 +0100

    feature: add solution for 2021 day 7

--- tests/test_year_2021.py
+++ tests/test_year_2021.py
@@ -12,6 +12,7 @@ from aoc.cli import get_solutions
         (2021, 4, 4512, 1924),
         (2021, 5, 5306, 17787),
         (2021, 6, 389726, 1743335992042),
+        (2021, 7, 344138, 94862124),
     ],
 )
 def test_solutions(year, day, expected_one, expected_two):
